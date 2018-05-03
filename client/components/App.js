1c16357bd26d14fb49cdaba328b470495b15b95f
Ola <olarpringo@gmail.com>
2018-05-04 00:18:17 +0100

    initial commit

--- client/components/App.js
+++ client/components/App.js
@@ -1,20 +1,50 @@
-import React, { Component } from 'react';
-import '../css/App.css';
+//client/components/App.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import Add from './Add'
+import Update from './Update'
+import Delete from './Delete';
+import { Tab, Tabs } from 'react-bootstrap';
+import YearTabsRouter from './tabs/yearTabsRouter';
+import MonthTabs from './tabs/monthTabs';
 
-class App extends Component {
-  render() {
+export default class App extends React.Component {
+constructor() {
+    super();
+  this.state = {selectedMonth:'All', selectedYear: 2016, data: []};
+    this.getData = this.getData.bind(this);
+  }
+componentDidMount() {
+    this.getData(this, '2016');
+  }
+  componentWillReceiveProps(nextProps) {
+    this.getData(this, '2016');
+  }
+getData(ev, year){
+    axios.get('/getAll?month=All&year='+year)
+      .then(function(response) {
+        ev.setState({data: response.data});
+        ev.setState({selectedYear: parseInt(year)})
+      });
+  }
+render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Add selectedMonth={this.state.selectedMonth} selectedYear={this.state.selectedYear} />
+        <table>
+          <thead>
+            <tr><th></th><th className='desc-col'>Description</th><th className='button-col'>Amount</th><th className='button-col'>Month</th><th className='button-col'>Year</th><th className='button-col'>Update</th></tr>
+          </thead>
+          <tbody>
+            {
+              this.state.data.map(function(exp){
+                return  <tr><td className='counterCell'></td><td className='desc-col'>{exp.description}</td><td className='button-col'>{exp.amount}</td><td className='button-col'>{exp.month}</td><td className='button-col'>{exp.year}</td><td className='button-col'><Update expense={exp} /></td></tr>
+              })
+            }
+            </tbody>
+</table>
       </div>
     );
   }
 }
-
-export default App;
